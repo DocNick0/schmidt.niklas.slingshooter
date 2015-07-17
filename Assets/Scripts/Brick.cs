cbec0c0b90875bd10486be3edd28954c52357126
Niklas Schmidt <niklasschmidt.ns@web.de>
2015-07-17 21:08:12 +0200

    no message

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+	
+
+	public float Health = 100f;
+
+	void OnCollisionEnter(Collision col) {
+
+		if (col.gameObject.tag == "Projectile") {
+
+		float damage = col.gameObject.GetComponent<Rigidbody> ().velocity.magnitude * 10;
+
+		//decrease health according to magnitude of object that hits brick
+		Health -= damage;
+
+		//if health is 0, destroy brick
+		if (Health <= 0)
+
+			Destroy (this.gameObject);
+		}
+
+	}
+}
---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+	// Static field accessible from anywhere
+	// Storing if the goal was met
+	public static bool goalMet = false;
+
+	public GameObject prefabGoalDead;
+
+	private GameObject goalDead;
+
+	public GameObject explosion;
+	
+
+	// Trigger got entered
+	void OnTriggerEnter(Collider other) {
+		// Check if the object entering the trigger is a tnt
+		if(other.gameObject.tag == "TNT") {
+
+			goalMet = true;
+
+			Instantiate (prefabGoalDead, gameObject.transform.position, gameObject.transform.rotation);
+
+			Instantiate (explosion, transform.position, transform.rotation);
+
+			Destroy (this.gameObject);
+
+
+		}
+	
+	}
+
+
+}
